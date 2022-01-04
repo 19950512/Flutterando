9e4a6bc7d2e044629f06f9a8111c35ea81fa3c47
Matheus <developer.web42@gmail.com>
2022-01-04 18:03:05 -0300

    04/01/2022 - Aula 03

---
+++ MasterClass/Aula_03/animacoes/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/dev/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/Flutterando/MasterClass/Aula_03/animacoes" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/dev/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/Flutterando/MasterClass/Aula_03/animacoes"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/Flutterando/MasterClass/Aula_03/animacoes/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/Flutterando/MasterClass/Aula_03/animacoes/lib/main.dart"
+)
